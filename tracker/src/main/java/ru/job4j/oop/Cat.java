e0a658513055c5b4a55c9222113f4e1339758756
SereginSun <sereginsun@yandex.ru>
2020-11-30 09:59:33 +0300

    Object fields.

--- tracker/src/main/java/ru/job4j/oop/Cat.java
+++ tracker/src/main/java/ru/job4j/oop/Cat.java
@@ -1,16 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+    private String food;
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.printf("%s cat eats a %s.%n", this.name, this.food);
+    }
+
+    public void eat(String food) {
+        this.food = food;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("cutlet");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
