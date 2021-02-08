a4efc64f126c9d7f1d5fb766b26c0ceb37095583
SereginSun <sereginsun@yandex.ru>
2021-02-08 22:59:19 +0300

    Combined comparator.

--- tracker/src/main/java/ru/job4j/collection/JobSorter.java
+++ tracker/src/main/java/ru/job4j/collection/JobSorter.java
@@ -2,19 +2,24 @@ package ru.job4j.collection;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class JobSorter {
     public static void main(String[] args) {
         List<Job> jobs = Arrays.asList(
+                new Job("Fix bugs", 1),
                 new Job("Fix bugs", 4),
-                new Job("Impl task", 2),
-                new Job("Reboot server", 1)
+                new Job("Fix bugs", 2),
+                new Job("X task", 0)
         );
         System.out.println(jobs);
         Collections.sort(jobs);
         System.out.println(jobs);
-        jobs.sort(new SortByNameJob());
+        Comparator<Job> comb = new JobDescByNameLn()
+                .thenComparing(new JobDescByName())
+                .thenComparing(new JobDescByPriority());
+        jobs.sort(comb);
         System.out.println(jobs);
     }
 }
