fa0de1e76a0a18feda1bd631e490dca2c4c0019c
SereginSun <sereginsun@yandex.ru>
2021-02-27 16:44:05 +0300

    Refactoring.

--- tracker/src/main/java/ru/job4j/oop/Fox.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.oop;
-
-public class Fox {
-
-    public void tryEat(Ball ball) {
-        System.out.println("I am hare.I will eat you!");
-    }
-}
--- tracker/src/main/java/ru/job4j/poly/Boat.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.poly;
-
-public class Boat implements Vehicle {
-    @Override
-    public void move() {
-        System.out.println(getClass().getSimpleName() + " floats on water.");
-    }
-}
